27ed781b3aa7c8819d2ef7206c2174d20d6a36ea
rimonamdadul-collab <rimonamdadul@gmail.com>
2026-03-13 11:53:48 +0600

    added the assignment-5

---
+++ js/box.js
@@ -0,0 +1,124 @@
+document.getElementById('ShopEase').addEventListener('click',
+    function(event){
+        const ShopEaseDiv=document.getElementById('ShopEase-div')
+        const p=document.createElement('p')
+        const time = new Date().toLocaleTimeString()
+        p.innerText=`You have Complete The Task Add Dark Mode at ${time}`;
+        ShopEaseDiv.appendChild(p)
+        p.classList.add('bg-[#F4F7FF] rounded-lg text-sm')
+
+        const sum=document.getElementById('number-sum').innerText;
+        let convertedSum=parseInt(sum)
+        convertedSum+1
+        const diff=document.getElementById('number-diff').innerText;
+        let convertedDiff=parseInt(diff)
+        convertedDiff-1
+        
+
+        
+    }
+)
+document.getElementById('btn-clear').addEventListener('click',function(){
+    const btn=getElementById('btn-clear').innerText;
+btn.innerText=''
+})
+
+document.getElementById('CloudSync').addEventListener('click',
+    function(event){
+        const ShopEaseDiv=document.getElementById('ShopEase-div')
+        const p=document.createElement('p')
+        const time = new Date().toLocaleTimeString()
+        p.innerText=`You have Complete The Task Add Dark Mode at ${time}`;
+        ShopEaseDiv.appendChild(p)
+        p.classList.add('bg-[#F4F7FF] rounded-lg text-sm')
+
+        const sum=document.getElementById('number-sum').innerText;
+        let convertedSum=parseInt(sum)
+        convertedSum+1
+        const diff=document.getElementById('number-diff').innerText;
+        let convertedDiff=parseInt(diff)
+        convertedDiff-1
+
+        
+    }
+)
+
+document.getElementById('Optimize').addEventListener('click',
+    function(event){
+        const ShopEaseDiv=document.getElementById('ShopEase-div')
+        const p=document.createElement('p')
+        const time = new Date().toLocaleTimeString()
+        p.innerText=`You have Complete The Task Add Dark Mode at ${time}`;
+        ShopEaseDiv.appendChild(p)
+        p.classList.add('bg-[#F4F7FF] rounded-lg text-sm')
+
+        const sum=document.getElementById('number-sum').innerText;
+        let convertedSum=parseInt(sum)
+        convertedSum+1
+        const diff=document.getElementById('number-diff').innerText;
+        let convertedDiff=parseInt(diff)
+        convertedDiff-1
+
+        
+    }
+)
+
+document.getElementById('Meta').addEventListener('click',
+    function(event){
+        const ShopEaseDiv=document.getElementById('ShopEase-div')
+        const p=document.createElement('p')
+        const time = new Date().toLocaleTimeString()
+        p.innerText=`You have Complete The Task Add Dark Mode at ${time}`;
+        ShopEaseDiv.appendChild(p)
+        p.classList.add('bg-[#F4F7FF] rounded-lg text-sm')
+
+        const sum=document.getElementById('number-sum').innerText;
+        let convertedSum=parseInt(sum)
+        convertedSum+1
+        const diff=document.getElementById('number-diff').innerText;
+        let convertedDiff=parseInt(diff)
+        convertedDiff-1
+
+        
+    }
+)
+
+document.getElementById('Fix').addEventListener('click',
+    function(event){
+        const ShopEaseDiv=document.getElementById('ShopEase-div')
+        const p=document.createElement('p')
+        const time = new Date().toLocaleTimeString()
+        p.innerText=`You have Complete The Task Add Dark Mode at ${time}`;
+        ShopEaseDiv.appendChild(p)
+        p.classList.add('bg-[#F4F7FF] rounded-lg text-sm')
+
+        const sum=document.getElementById('number-sum').innerText;
+        let convertedSum=parseInt(sum)
+        convertedSum+1
+        const diff=document.getElementById('number-diff').innerText;
+        let convertedDiff=parseInt(diff)
+        convertedDiff-1
+
+        
+    }
+)
+
+document.getElementById('ShopEase').addEventListener('click',
+    function(event){
+        const ShopEaseDiv=document.getElementById('ShopEase-div')
+        const p=document.createElement('p')
+        const time = new Date().toLocaleTimeString()
+        p.innerText=`You have Complete The Task Add Dark Mode at ${time}`;
+        ShopEaseDiv.appendChild(p)
+        p.classList.add('bg-[#F4F7FF] rounded-lg text-sm')
+
+        const sum=document.getElementById('number-sum').innerText;
+        let convertedSum=parseInt(sum)
+        convertedSum+1
+        const diff=document.getElementById('number-diff').innerText;
+        let convertedDiff=parseInt(diff)
+        convertedDiff-1
+    
+        
+    }
+)
---
+++ js/togglle.js
@@ -0,0 +1,6 @@
+document.getElementById('Discover').addEventListener('click',
+    function(event){
+        event.preventDefault()
+        window.location.href='./toggle.html'
+    }
+)
